7812e1f7904012faaf984765c8eb94be8f67a16d
xcl <chunlaixiao@163.com>
2019-04-05 11:58:25 +0800

    add configs file

---
+++ www/configs_default.py
@@ -0,0 +1,14 @@
+#默认配置文件
+
+configs={
+    'db':{
+        'host':'0.0.0.0',
+        'port':3306, #mysql 默认的端口
+        'user':'xcl',
+        'password':'mysql608213',
+        'database':'awesome'
+    },
+    'session':{
+         'secret': 'AwEsOmE'
+    }
+}
